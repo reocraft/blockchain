b67bb986d264ba55a6d14e0243e4aee1cfbb1680
reocraft <reocraft.creeper@gmail.com>
2025-10-12 14:11:09 -0500

    Worked on Hash.java and Block.java

--- src/main/java/edu/grinnell/csc207/blockchain/Block.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Block.java
@@ -1,8 +1,88 @@
 package edu.grinnell.csc207.blockchain;
 
+import java.nio.ByteBuffer;
+import java.security.DigestException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * A single block of a blockchain.
  */
 public class Block {
     // TODO: fill me in!
+    private final int num;
+    private final int amount;
+    private final Hash prevHash;
+    private long nonce;
+    private Hash hash;
+
+    public Block(int num, int amount, Hash prevHash) throws NoSuchAlgorithmException{
+        this.num = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+        // nonce = we will make method to calculate
+        long nonce = 0;
+        Hash computed;
+        while (true) {
+            computed = calculateHash(num, amount, prevHash, nonce);
+            if (isValidHash(computed)) {
+                break;
+            }
+            nonce++;
+        }
+        this.nonce = nonce;
+        this.hash = calculateHash(num, amount, prevHash, nonce);
+    }
+
+    public Block(int num, int amount, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
+        this.num = num;
+        this.amount = amount;
+        this.prevHash = prevHash;
+        this.nonce = nonce;
+        this.hash = calculateHash(num, amount, prevHash, nonce);
+    }
+
+    public int getNum() {
+        return num;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public long getNonce() {
+        return nonce;
+    }
+    public Hash getPrevHash() {
+        return prevHash;
+    }
+    public Hash getHash() {
+        return hash;
+    }
+ 
+    public static Hash calculateHash(int num, int amount, Hash prevHash, long nonce) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(ByteBuffer.allocate(4).putInt(num).array());
+        md.update(ByteBuffer.allocate(4).putInt(amount).array());
+        if (prevHash != null) {
+            md.update(prevHash.getData());
+        }
+        md.update(ByteBuffer.allocate(8).putLong(nonce).array());
+        byte[] digest = md.digest();
+        return new Hash(digest);
+    }
+
+    private static boolean isValidHash(Hash h) {
+        byte[] bytes = h.getData();
+        return bytes[0] == 0 && bytes[1] == 0 && bytes[2] == 0;
+    }
+
+
+    public String toString() {
+        String prevHashStr;
+        if (prevHash == null) {
+            prevHashStr = "null";
+        } else {
+            prevHashStr = prevHash.toString();
+        }
+        return ("Block " + num + " (Amount: " + amount + ", Nonce: " + nonce + ", prevHash: " + prevHashStr + ", hash: " + hash.toString());
+    }
 }
--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -1,8 +1,60 @@
 package edu.grinnell.csc207.blockchain;
 
+import java.util.Arrays;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * A wrapper class over a hash value (a byte array).
  */
 public class Hash {
     // TODO: fill me in!
+    private byte[] data;
+
+    public Hash (byte[] data) {
+        this.data = Arrays.copyOf(data, data.length);
+    }
+
+    public byte[] getData() {
+        return Arrays.copyOf(data, data.length);
+    }
+
+    public boolean isValid() {
+        if (data.length < 3) {
+            return false;
+        }
+        for (int i = 0; i < 3; i++) {
+            byte mask = (byte) (1 << i);
+            if ((data[i] & mask) == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            sb.append(String.format("%02x", Byte.toUnsignedInt(data[i])));
+        }
+        return sb.toString();
+    }
+
+    public boolean equals (Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other != null && (other instanceof Hash)) {
+            return true;
+        }
+        Hash o = (Hash) other;
+        return Arrays.equals(this.data, o.data);
+    }
+
+    public static byte[] calculateHash(String msg) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("sha-256");
+        md.update(msg.getBytes());
+        byte[] hash = md.digest();
+        return hash;
+    }
 }
